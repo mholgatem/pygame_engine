0c9315446c357da339f6e2054d09566442bd4d28
mholgatem <holgate.mark1@gmail.com>
2015-03-31 23:36:06 +0000

    code cleanup

--- editor.py
+++ editor.py
@@ -6,26 +6,12 @@ import os
 
 class Editor(object):
 	def __init__(self):
-		self.running = True
-		self.timer = pygame.time.Clock()
-		self.Game = game.Game()
-		self.screen = self.Game.screen
-		self.update_rects = []
+		#setup initial scene
+		self.INIT_GAME()
 		
-		self.Game.set_background('assets/images/background.jpg')
-		self.Game.frames_per_second = 60
-		
-		self.layer_objects = pygame.sprite.LayeredUpdates() #ALL OBJECTS EXCEPT BACKGROUND
-		self.buttons = pygame.sprite.LayeredUpdates() 
-		self.popup_buttons = pygame.sprite.LayeredUpdates()
-		self.frames = pygame.sprite.LayeredUpdates() 
-		self.animated = pygame.sprite.Group() #SPRITES THAT HAVE ANIMATION
-		
-		self.tool_bar = pygame.Rect(self.Game.screen_width - 150, 0, 150, self.Game.screen_height)
-		self.sprite_work_area = pygame.Rect(0,0, self.Game.screen_width - 150, self.Game.screen_height)
-		self.spritesheet = None
-		self.run = None
-		self.selected_frame = None
+		#set sprite groups & variables
+		self.INIT_SPRITE_GROUPS()
+		self.INIT_VARIABLES()
 		
 		#draw tool_bar to clean background, so we can avoid re-drawing every time we clean the background.
 		self.Game.clean_background.fill((80,80,80), self.tool_bar)
@@ -33,8 +19,32 @@ class Editor(object):
 		self.screen.blit(self.Game.background, (0,0))
 		pygame.display.flip()
 		
-		self.path_to_assets = os.path.realpath('./assets/images/')
 		self.Main()
+	
+	def INIT_GAME(self, fps = 60):
+		self.running = True
+		self.timer = pygame.time.Clock()
+		self.Game = game.Game()
+		self.screen = self.Game.screen
+		self.update_rects = []
+		
+		self.Game.set_background('assets/images/background.jpg')
+		self.Game.frames_per_second = fps
+	
+	def INIT_SPRITE_GROUPS(self):
+		self.layer_objects = pygame.sprite.LayeredUpdates() #ALL OBJECTS EXCEPT BACKGROUND
+		self.buttons = pygame.sprite.LayeredUpdates() 
+		self.popup_buttons = pygame.sprite.LayeredUpdates()
+		self.frames = pygame.sprite.LayeredUpdates() 
+		self.animated = pygame.sprite.Group() #SPRITES THAT HAVE ANIMATION
+	
+	def INIT_VARIABLES(self):
+		self.tool_bar = pygame.Rect(self.Game.screen_width - 150, 0, 150, self.Game.screen_height)
+		self.sprite_work_area = pygame.Rect(0,0, self.Game.screen_width - 150, self.Game.screen_height)
+		self.spritesheet = None
+		self.sample = None
+		self.selected_frame = None
+		self.path_to_assets = os.path.realpath('./assets/images/')
 	
 	def rect_from_point(self, pos1, pos2 = None):
 		if not pos2: pos2 = pos1
@@ -48,14 +58,14 @@ class Editor(object):
 		self.rects = [frame.rect for frame in self.frames]
 		if self.rects:
 			return game.Animator(title = "run", 
-											sprite_sheet = self.spritesheet.scaled_image, 
-											number_of_frames = len(self.frames),
-											animation_area = self.spritesheet.rect,#self.rects[0].unionall(self.rects), 
-											frame_list = self.rects, 
-											speed = 100,
-											loop = True, 
-											loop_start_frame = 0)
-											#test = True)
+					sprite_sheet = self.spritesheet.scaled_image, 
+					number_of_frames = len(self.frames),
+					animation_area = self.spritesheet.rect,#self.rects[0].unionall(self.rects), 
+					frame_list = self.rects, 
+					speed = 100,
+					loop = True, 
+					loop_start_frame = 0)
+					#test = True)
 		return None
 	
 	def open_file(self, path = None):
@@ -92,41 +102,30 @@ class Editor(object):
 		
 		return False
 	
-	def zoom_in(self):
-		self.scale += 1
-		for frame in self.frames:
-			frame.rect.move_ip((frame.rect.x)  , frame.rect.y)
-			topleft = frame.rect.topleft
-			frame.rect.inflate_ip(frame.rect.w, frame.rect.h )
-			frame.rect.topleft = topleft
-			frame.move_and_update(0,0)
-		self.Game.background = self.Game.clean_background.copy()
-		if self.spritesheet:
-			self.spritesheet.rect.inflate_ip(self.spritesheet.rect.w, self.spritesheet.rect.h)
-			self.spritesheet.rect.topleft = (0,0)
-			try: self.spritesheet.scaled_image = pygame.transform.smoothscale(self.spritesheet.image, (self.spritesheet.rect.w, self.spritesheet.rect.h))
-			except: self.spritesheet.scaled_image = pygame.transform.scale(self.spritesheet.image, (self.spritesheet.rect.w, self.spritesheet.rect.h))
-			self.Game.background.blit(self.spritesheet.scaled_image, self.sprite_work_area.topleft)
-		self.screen.blit(self.Game.background, (0,0))
-		pygame.display.flip()
-	
-	def zoom_out(self):
-		self.scale -= 1
-		for frame in self.frames:
-			frame.rect.move_ip((frame.rect.x * -.5) , (frame.rect.y  * -.5) )
-			topleft = frame.rect.topleft
-			frame.rect.inflate_ip((frame.rect.w  * -.5) , (frame.rect.h  * -.5) )
-			frame.rect.topleft = topleft
-			frame.move_and_update(0,0)
-		self.Game.background = self.Game.clean_background.copy()
-		if self.spritesheet:
-			self.spritesheet.rect.inflate_ip((self.spritesheet.rect.w  * -.5) , (self.spritesheet.rect.h  * -.5) )
-			self.spritesheet.rect.topleft = (0,0)
-			try: self.spritesheet.scaled_image = pygame.transform.smoothscale(self.spritesheet.image, (self.spritesheet.rect.w, self.spritesheet.rect.h))
-			except: self.spritesheet.scaled_image = pygame.transform.scale(self.spritesheet.image, (self.spritesheet.rect.w, self.spritesheet.rect.h))
-			self.Game.background.blit(self.spritesheet.scaled_image, self.sprite_work_area.topleft)
-		self.screen.blit(self.Game.background, (0,0))
-		pygame.display.flip()
+	def zoom(self,zoom):
+		if zoom.upper() == 'IN':
+			self.scale += 1
+			amt = 1
+		if zoom.upper() == 'OUT':
+			self.scale -= 1
+			amt = -0.5
+		if zoom.upper() in ('IN', 'OUT'):
+			for frame in self.frames:
+				frame.rect.move_ip((frame.rect.x * amt) , (frame.rect.y  * amt) )
+				topleft = frame.rect.topleft
+				frame.rect.inflate_ip((frame.rect.w  * amt) , (frame.rect.h  * amt) )
+				frame.rect.topleft = topleft
+				frame.move_and_update(0,0)
+			self.Game.background = self.Game.clean_background.copy()
+			if self.spritesheet:
+				self.spritesheet.rect.inflate_ip((self.spritesheet.rect.w  * amt) , (self.spritesheet.rect.h  * amt) )
+				self.spritesheet.rect.topleft = (0,0)
+				try: self.spritesheet.scaled_image = pygame.transform.smoothscale(self.spritesheet.image, (self.spritesheet.rect.w, self.spritesheet.rect.h))
+				except: self.spritesheet.scaled_image = pygame.transform.scale(self.spritesheet.image, (self.spritesheet.rect.w, self.spritesheet.rect.h))
+				self.Game.background.blit(self.spritesheet.scaled_image, self.sprite_work_area.topleft)
+			self.screen.blit(self.Game.background, (0,0))
+			pygame.display.flip()
+
 	
 	def get_item_clicked(self, group, pos):
 		for item in group:
@@ -149,7 +148,7 @@ class Editor(object):
 				self.selected_frame.move_and_update(0, 1)
 			refresh_animation = [True for event in events if event.type == pygame.KEYUP and 273 <= event.key <= 276]
 			if refresh_animation and self.spritesheet:
-				self.run = self.get_animation()
+				self.sample = self.get_animation()
 			
 		for event in self.mouse_events:
 
@@ -169,10 +168,10 @@ class Editor(object):
 				if event.type == pygame.MOUSEBUTTONDOWN:
 					#ZOOM IN
 					if event.button == 4 and self.scale < 3:
-						self.zoom_in()
+						self.zoom('IN')
 					#ZOOM OUT
 					if event.button == 5 and self.scale >= 0:
-						self.zoom_out()
+						self.zoom('OUT')
 					#GRAB CORNER/CREATE NEW FRAME
 					if pygame.mouse.get_pressed()[0]:
 						self.grab_frame = self.grab_corner(pygame.mouse.get_pos(), self.frames)
@@ -199,7 +198,7 @@ class Editor(object):
 						self.grab_frame = False
 					#UPDATE PREVIEW ANIMATION
 					if self.spritesheet:
-						self.run = self.get_animation()
+						self.sample = self.get_animation()
 						
 				#RESIZE FRAME
 				if pygame.mouse.get_pressed()[0] and event.type == pygame.MOUSEMOTION:
@@ -207,12 +206,13 @@ class Editor(object):
 						self.clear_area = self.grab_frame['frame'].rect.inflate(2,2)
 						self.screen.blit(self.Game.background, self.clear_area, self.clear_area)
 						mouse_x, mouse_y = pygame.mouse.get_pos()
-						x_values = [x for frame in self.frames if frame is not self.grab_frame['frame'] for x in (frame.rect.left, frame.rect.right) if x-10 < mouse_x < x +10]
-						y_values = [y for frame in self.frames if frame is not self.grab_frame['frame'] for y in (frame.rect.top, frame.rect.bottom) if y-10 < mouse_y < y +10]
-						if x_values:mouse_x = x_values[0]
-						if y_values:mouse_y = y_values[0]
-
-								
+						#SNAP EDGES
+						if not pygame.key.get_mods() & pygame.KMOD_LSHIFT:
+							x_values = [x for frame in self.frames if frame is not self.grab_frame['frame'] for x in (frame.rect.left, frame.rect.right) if x-10 < mouse_x < x +10]
+							y_values = [y for frame in self.frames if frame is not self.grab_frame['frame'] for y in (frame.rect.top, frame.rect.bottom) if y-10 < mouse_y < y +10]
+							if x_values: mouse_x = x_values[0]
+							if y_values: mouse_y = y_values[0]
+						
 						self.grab_frame['corner'].center = (mouse_x, mouse_y)
 						self.grab_frame['frame'].update(corner_index = self.grab_frame['index'])
 
@@ -246,13 +246,13 @@ class Editor(object):
 			if self.selected_frame in self.frames.sprites(): pygame.draw.rect(self.screen, (0,255,255), self.selected_frame.rect, 1)
 			
 			
-			if self.run: 
-				self.screen.fill((80,80,80), self.run.rect)
-				self.update_rects.append(self.run.rect)
-				self.run.update()
-				self.run.rect.top = 200
-				self.screen.blit(self.run.image, (0,200))
-				self.update_rects.append(self.run.rect)
+			if self.sample: 
+				self.screen.fill((80,80,80), self.sample.rect)
+				self.update_rects.append(self.sample.rect)
+				self.sample.update()
+				self.sample.rect.top = 200
+				self.screen.blit(self.sample.image, (0,200))
+				self.update_rects.append(self.sample.rect)
 				
 			#self.popup_buttons.clear(self.screen, self.Game.background_image)
 			#self.popup_buttons.draw(self.screen)
